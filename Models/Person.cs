ab85338089afdeff3e1ad195b3cf1e24cb299de4
Aiyaar <Omarbangash8@gmail.com>
2020-08-31 17:02:50 +0500

    first commit

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Demo_WebApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
